ad363fc799937518cd3efafae5cb23d2fa5ea8d8
clinton moshe mwachia <clintonmwachia9@gmail.com>
2022-05-27 12:27:45 +0300

    Added analysis and data page.

--- app.R
+++ app.R
@@ -1,6 +1,29 @@
 library(shiny)
 library(semantic.dashboard)
 
+data_page <- (
+  fluidRow(
+    box(
+      title = "Filters",width = 5, collapsible=FALSE,
+      ribbon = FALSE,
+      h3("data filters")
+    ),
+    box(
+      title = "Output", width = 11, collapsible=FALSE,
+      ribbon = FALSE,
+      h3("Data output")
+    )
+  )
+)
+
+analysis_page <- (
+  box(
+    title = "Analysis", width = 16, collapsible=FALSE,
+    ribbon = FALSE,
+    h3("Data Analysis")
+  )
+)
+
 ui <- dashboardPage(
   dashboardHeader(title = "Awesome Dashboard"),
   dashboardSidebar(
@@ -13,11 +36,11 @@ ui <- dashboardPage(
     tabItems(
       tabItem(
         tabName = "Data",
-        h2("data")
+        data_page
       ),
       tabItem(
         tabName = "Analysis",
-        h2("analysis")
+        analysis_page
       )
     )
   )
