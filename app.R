c2da66da39f44b30d1e2b7c1d13d01ece3225018
clinton moshe mwachia <clintonmwachia9@gmail.com>
2022-06-02 13:51:43 +0300

    Added four plots to the scatter plot tabs.

--- app.R
+++ app.R
@@ -1,6 +1,8 @@
 # loading the libraries
 library(shiny)
+library(ggplot2)
 library(dplyr)
+library(plotly)
 library(semantic.dashboard)
 
 
@@ -21,34 +23,59 @@ numeric_cols <- c("mpg", "disp", "hp", "drat" , "wt" , "qsec")
 categoric_cols <- c("vs", "am", "gear", "carb", "cyl")
 
 data_page <- (
+    fluidRow(
+      box(
+        title = "Filters",width = 5, collapsible=FALSE,
+        ribbon = FALSE, title_side = "top left",
+        color = "green",
+        #selectInput("col1","column one", choices = numeric_cols),
+        #selectInput("col2","column two", choices = numeric_cols)
+        selectInput("vs", "select Engine", choices = unique(data$vs), 
+                    selected = 0),
+        selectInput("am", "select Transmission", choices = NULL),
+        selectInput("gear", "select No. of gears", choices = unique(data$gear), 
+                    selected = 1)
+      ),
+      box(
+        title = "Output", width = 11, collapsible=FALSE,
+        ribbon = FALSE, title_side = "top left",
+        color = "teal",
+        tableOutput("table")
+      )
+    )
+)
+
+scatter_plot_page <- (
   fluidRow(
     box(
-      title = "Filters",width = 5, collapsible=FALSE,
+      title = "scatter plot with labels", width = 8, collapsible=FALSE,
       ribbon = FALSE, title_side = "top left",
-      color = "green",
-      #selectInput("col1","column one", choices = numeric_cols),
-      #selectInput("col2","column two", choices = numeric_cols)
-      selectInput("vs", "select Engine", choices = unique(data$vs), 
-                  selected = 0),
-      selectInput("am", "select Transmission", choices = NULL),
-      selectInput("gear", "select No. of gears", choices = unique(data$gear), 
-                  selected = 1)
+      color = "olive",
+      plotOutput("sp1")
     ),
     box(
-      title = "Output", width = 11, collapsible=FALSE,
+      title = "interactive plotly", width = 8, collapsible=FALSE,
       ribbon = FALSE, title_side = "top left",
-      color = "teal",
-      tableOutput("table")
+      color = "olive",
+      plotlyOutput("sp2")
     )
   )
 )
 
-analysis_page <- (
-  box(
-    title = "Analysis", width = 16, collapsible=FALSE,
-    ribbon = FALSE, title_side = "top left",
-    color = "olive",
-    h3("Data Analysis")
+scatter_plot_page1 <- (
+  fluidRow(
+    box(
+      title = "scatter plot by groups", width = 8, collapsible=FALSE,
+      ribbon = FALSE, title_side = "top left",
+      color = "olive",
+      plotOutput("spg")
+    ),
+    box(
+      title = "scatter ploy by groups and size", width = 8, collapsible=FALSE,
+      ribbon = FALSE, title_side = "top left",
+      color = "olive",
+      plotOutput("spg2")
+    )
   )
 )
 
@@ -57,18 +84,19 @@ ui <- dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem(tabName = "Data", "data"),
-      menuItem(tabName = "Analysis", "analysis")
+      menuItem(tabName = "Analysis", "Scatter Plot")
     )
   ),
   dashboardBody(
     tabItems(
       tabItem(
         tabName = "Data",
-        data_page
+        data_page,
       ),
       tabItem(
         tabName = "Analysis",
-        analysis_page
+        scatter_plot_page,
+        scatter_plot_page1
       )
     )
   )
@@ -96,6 +124,54 @@ server <- function(input, output, session) {
   output$table <- renderTable({
     home_data()
   })
+  
+  output$sp1 <- renderPlot({
+    data <- data %>%
+      head()
+    
+    data %>%
+      head(30) %>%
+      ggplot(aes(x=wt, y=mpg)) +
+      geom_point() +
+      geom_text( 
+        label=rownames(data),
+        nudge_x = 0.25, nudge_y = 0.25, 
+        check_overlap = T
+      )
+  })
+  
+  output$sp2 <- renderPlotly({
+    data <- data %>%
+      head()
+    
+    p <- data %>%
+      head(30) %>%
+      ggplot(aes(x=wt, y=mpg)) +
+      geom_point() +
+      geom_text( 
+        label=rownames(data),
+        nudge_x = 0.25, nudge_y = 0.25, 
+        check_overlap = T
+      )
+    ggplotly(p)
+  })
+  
+  output$spg <- renderPlot({
+    
+    iris %>%
+      ggplot(aes(x=Sepal.Length, y=Sepal.Width, color=Species)) + 
+      geom_point(size=6) +
+      theme_bw()
+  })
+  
+  output$spg2 <- renderPlot({
+    
+    iris %>%
+      ggplot(aes(x=Sepal.Length, y=Sepal.Width, shape=Species,color=Species)) + 
+      geom_point(size=6) +
+      theme_bw()
+  })
+  
 }
 
 shinyApp(ui, server)
